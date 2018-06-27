f55756c7b661278c3c1ad6b1b9c473051aaae2e9
Anton Zekeriev Rodin <antonzekeriev@gmail.com>
2018-06-27 09:24:07 +0200

    The Camel Case Solution

---
+++ camel-case/app.js
@@ -0,0 +1,37 @@
+function spinalCase(str) {
+    
+    //StripTags from HTML
+    let re = /<\/?[^>]+(>|$)/g;
+    str = str.replace(re, "");
+    let result = "";
+    let prev = "";
+    let curr = "";
+    let i = 0;
+
+    while(i < str.length) {
+        curr = str[i];
+        curr = (curr == "_") ? "-" : curr;
+
+        if (curr != curr.toLowerCase()) {
+            if(prev != "-" && prev != "") {
+                curr = "-" + curr.toLowerCase();
+            } else {
+                curr = curr.toLowerCase();
+            }
+        } else if (curr == " ") {
+            curr = "-";
+        }
+
+        prev = curr;
+        result += curr;
+        i++;
+    }
+
+    console.log(result);
+    return result;
+}
+
+spinalCase('Teletubbies say Eh-oh');
+spinalCase('This Is Spinal Tap');
+spinalCase('The_Andy_Griffith_Show');
+spinalCase('thisIsSpinal<wbr>Tap');
